39dacc19de63050018f589eb4af1187b5ed9524a
qsy <1791538409@qq.com>
2025-12-31 14:24:33 +0800

    Prepare to Test DPDK Message v15

--- src/test/msgr/perf_msgr_client.cc
+++ src/test/msgr/perf_msgr_client.cc
@@ -60,7 +60,7 @@ class MessengerClient {
       case CEPH_MSG_OSD_OPREPLY:
         return thread->check_cid(m->get_tid());
       default:
-        return false;
+        return true;
       }
     }
 
@@ -414,6 +414,7 @@ void MessengerClient::ClientDispatcher::ms_fast_dispatch(Message *m) {
     thread->cond.notify_all();
   }
   else{
+    std::cout<<"dpdk recv message, tid:"<<m->get_tid()<<std::endl;
     thread->set_record(m->get_tid(),recv_time);
     thread->inflight--;
   }
