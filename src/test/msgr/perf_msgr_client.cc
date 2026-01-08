df4eaaac2256d23068ea1067fc7a0cd08dc855f9
qsy <1791538409@qq.com>
2026-01-08 19:18:23 +0800

    to fix memory release problem v1

--- src/test/msgr/perf_msgr_client.cc
+++ src/test/msgr/perf_msgr_client.cc
@@ -484,8 +484,8 @@ int main(int argc, char **argv)
       g_ceph_context->_conf.set_val("ms_dpdk_force_zero_copy", "false",&cout_ss);
     }
 
-    g_ceph_context->_conf.set_val("debug_dpdk", "20/20", &cout_ss);
-    g_ceph_context->_conf.set_val("debug_ms", "11/11", &cout_ss);
+    g_ceph_context->_conf.set_val("debug_dpdk", "1/1", &cout_ss);
+    g_ceph_context->_conf.set_val("debug_ms", "1/1", &cout_ss);
     cout<<cout_ss.str()<<std::endl;
   }
   common_init_finish(g_ceph_context);
