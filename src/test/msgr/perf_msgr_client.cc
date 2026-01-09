b27a4de0ed652420423e76d78106e35a29e913de
qsy <1791538409@qq.com>
2026-01-09 16:06:50 +0800

    Test DPDKMessage OK! Stage 2 is over--

--- src/test/msgr/perf_msgr_client.cc
+++ src/test/msgr/perf_msgr_client.cc
@@ -176,7 +176,7 @@ class MessengerClient {
           to_send_data_off +=to_copy;
           vecs.push_back(frag);
         }
-        std::cout<<"send dpdk message, tid:"<<dpdk_msg->get_tid()<<std::endl;
+        // std::cout<<"send dpdk message, tid:"<<dpdk_msg->get_tid()<<std::endl;
         dpdk_msg->set_data(Packet(vecs, deleter()));
         (*conns)[sid_index]->send_dpdk_message(dpdk_msg);
         inflight++;
@@ -418,7 +418,7 @@ void MessengerClient::ClientDispatcher::ms_fast_dispatch(Message *m) {
     thread->cond.notify_all();
   }
   else{
-    std::cout<<"dpdk recv message, tid:"<<m->get_tid()<<std::endl;
+    // std::cout<<"dpdk recv message, tid:"<<m->get_tid()<<std::endl;
     thread->set_record(m->get_tid(),recv_time);
     thread->inflight--;
   }
